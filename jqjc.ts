4014cc650c56b823de180cd879a498fbe6ca12b9
pureji <20034483+pureji1980@users.noreply.github.com>
2019-05-26 05:59:33 +0900

    Add files via upload
    mkdir tttt
    cd tttt
    npm install @types/jquery --save
    tsc jqjc.ts -m commonjs -t ES6
    
    localhost/tttt/jqjc.html

---
+++ jqjc.ts
@@ -0,0 +1,50 @@
+///<reference path="src/jcanvas.d.ts"/>
+// npm install --save @types/jquery  ←Jquery.d.tsの設定
+/* jQuery と jCanvas の型定義ファイルを設定することで、
+　 トランスパイルが通るようにする。
+   tsc -m commonjs -t ES6 jqjc.ts
+*/
+
+//jCanvas描画起点を中心からではなく左上からにする
+$.jCanvas.defaults.fromCenter = false;
+
+$(function(){
+   console.log("jQuery is alive!");
+   $(".test").text("testtest");
+
+   var d = new Draws(10,20,"#CCAC26");
+   d.draw();
+})
+
+class Draws{
+   public x:number;
+   public y:number;
+   public c:string;
+
+   constructor(x?:number,y?:number,c?:string){
+      this.x = x || 0;
+      this.y = y || 0;
+      this.c = c || "#000000";
+   }
+
+   draw(){
+      $("#canv").drawRect({
+         fillStyle: this.c,
+         x: this.x, y: this.y,
+         width: 200,
+         height: 100,
+         draggable:true
+      });
+
+      $("#canv").drawText({
+         fillStyle: '#9cf',
+         strokeStyle: '#25a',
+         strokeWidth: 2,
+         x: 150, y: 100,
+         fontSize: 48,
+         fontFamily: 'Verdana, sans-serif',
+         text: 'Hello'
+      })
+
+   }
+}
---
+++ src/jcanvas.d.ts
@@ -0,0 +1,30 @@
+// jcanvas.d.ts
+// 2019.5.25 ぷれじ改
+// https://github.com/uestla/convex-hull/blob/master/ConvexHull/jcanvas.d.ts
+//
+// main.ts の先頭にて
+// <reference path="jcanvas.d.ts" /> を追記すること(トリプルスラッシュ)
+// なお、jQuery.d.tsは
+// npm install --save @types/jquery を推奨
+
+interface JQuery {
+	drawLayers(): JQuery;
+	clearCanvas(): JQuery;
+	removeLayers(): JQuery;
+	drawArc(options: any): JQuery;
+	drawLine(options: any): JQuery;
+	drawText(options: any): JQuery;
+	drawRect(options: any): JQuery; //なぜか無かったので追記
+  removeLayer(name: string): JQuery;
+	removeLayerGroup(name: string): JQuery;
+	setLayer(name: string, options: any): JQuery;
+	setLayerGroup(name: string, options: any): JQuery;
+
+	attr(name: string, value: boolean): JQuery;
+}
+
+//追記
+//$.jCanvas.defaults.fromCenter = false; を通すため
+interface JQueryStatic{
+	jCanvas:any;
+}
